7e06b0b99e255a7620e5f423169ee3bd1812d574
lmlynik <lukasz.mlynik@hotmail.com>
2018-06-11 13:23:28 +0200

    Updated tests

--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleExample
         static async Task MatchmoreExample()
         {
             // Configure Matchmore with your credentials
-            await Matchmore.SDK.Matchmore.ConfigureAsync("YOUR_API_KEY");
+            await Matchmore.SDK.Matchmore.ConfigureAsync("eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJhbHBzIiwic3ViIjoiMzIzMmY3NTYtMzkzYi00OWM2LTgxMjItMzBlNTI5NDZiOWVkIiwiYXVkIjpbIlB1YmxpYyJdLCJuYmYiOjE1MjIxNTk3NTUsImlhdCI6MTUyMjE1OTc1NSwianRpIjoiMSJ9.6Ay0Ollaf1Wl1WXSyvb6B0_62fr74TYYFV0VykmORAL0sELzAMBvAXukFEeVYwwGv5W99AL-qVwzTj2UWn05ig");
 
             //you can access the device later by calling Matchmore.SDK.Matchmore.Instance.MainDevice
             await Matchmore.SDK.Matchmore.Instance.SetupMainDeviceAsync();
--- Matchmore.SDK.Shared.Tests/TestBase.cs
+++ Matchmore.SDK.Shared.Tests/TestBase.cs
@@ -12,7 +12,7 @@ using Matchmore.SDK.Xamarin.Shared;
 namespace Matchmore.Tests
 {
 	public class TestBase
-	{   public string apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJhbHBzIiwic3ViIjoiMzIyNzliODEtODBkMS00MmFkLTgyNjEtNDE1MDlmNjM0NWRlIiwiYXVkIjpbIlB1YmxpYyJdLCJuYmYiOjE1MjY5MTE3NDYsImlhdCI6MTUyNjkxMTc0NiwianRpIjoiMSJ9.9ppoTeyGwhk9tERCThmJgXcenMvnggxWq53QTMxokyRi9AU0BETYPoBttdjPaPWhCvJpnxpercLATqt9nwt9ow";
+    {   public string apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJhbHBzIiwic3ViIjoiMzIzMmY3NTYtMzkzYi00OWM2LTgxMjItMzBlNTI5NDZiOWVkIiwiYXVkIjpbIlB1YmxpYyJdLCJuYmYiOjE1MjIxNTk3NTUsImlhdCI6MTUyMjE1OTc1NSwianRpIjoiMSJ9.6Ay0Ollaf1Wl1WXSyvb6B0_62fr74TYYFV0VykmORAL0sELzAMBvAXukFEeVYwwGv5W99AL-qVwzTj2UWn05ig";
 		protected static Matchmore.SDK.Matchmore _instance;
 
 		protected Subscription sub = new Subscription
